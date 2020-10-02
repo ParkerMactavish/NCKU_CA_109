8ecc53c9942bcbe518a6b3761f4f1ad627b535e4
Parker <e24066226@gs.ncku.edu.tw>
2020-10-02 17:57:03 +0800

    FEAT Finished all implementations

--- HW1/formatPrinter.s
+++ HW1/formatPrinter.s
@@ -1,6 +1,6 @@
 .data
 pChr: .string "StringInArg"
-str1: .string "%s\nnormalString\nCharInArg%c\nNot supporting: %e%e%r%d\nHex0: %x    %X\nHex1: %x    %X\nHex2: %x    %X\nOct: %o\n"
+str1: .string "%s\nnormalString\nCharInArg: %c\nPercentSign: %%\nNot supporting: %e%e%r%d\nHex0: %x    %X\nHex1: %x    %X\nHex2: %x    %X\nOct: %o\nBin: %b"
 
 .text
 main:
@@ -17,18 +17,21 @@ main:
     lui  s6, 0x3b14f
     addi s6, s6, 0x50f      # testBin = 0b00111011000101001111010100001111 = 0x3b14f50f
 
-    la   t0, str1           # "%s\nnormalString\nCharInArg%c\nNot supporting: %e%e%r%d\nHex0: %x    %X\nHex1: %x    %X\nHex2: %x    %X\nOct: %o\n"
-    addi sp, sp, -32
+    la   t0, str1           # "%s\nnormalString\nCharInArg%c\nNot supporting: %e%e%r%d\nHex0: %x    %X\nHex1: %x    %X\nHex2: %x    %X\nOct: %o\nBin: %b"
+    addi sp, sp, -44
     sw   t0, 0(sp)
     sw   s0, 4(sp)
     sw   s1, 8(sp)
     sw   s2, 12(sp)
-    sw   s3, 16(sp)
-    sw   s4, 20(sp)
-    sw   s5, 24(sp)
-    sw   s6, 28(sp)
+    sw   s2, 16(sp)
+    sw   s3, 20(sp)
+    sw   s3, 24(sp)
+    sw   s4, 28(sp)
+    sw   s4, 32(sp)
+    sw   s5, 36(sp)
+    sw   s6, 40(sp)
     jal  self_print
-    addi sp, sp, 32
+    addi sp, sp, 44
     addi a0, zero, 0        # return 0
     addi a7, zero, 93
     ecall
@@ -36,7 +39,9 @@ main:
 self_print:
     lw   t0, 0(sp)          # const char* format
     addi t1, sp, 4          # va_list ap; va_start(ap, format);
-
+    addi sp, sp, -40        # int shiftAmt;
+                            # int capFlag;
+                            # char digits[32];
     while_begin:
     lb   t2, 0(t0)          # chr = *format
     beq  t2, zero, while_end
@@ -62,14 +67,94 @@ self_print:
     case_s_end:
 
     addi t3, zero, 99
-    bne  t2, t3, case_c_end # case 's'
+    bne  t2, t3, case_c_end # case 'c'
     lw   a0, 0(t1)
     addi t1, t1, 4          # (char)va_arg(ap, int)
     addi a7, zero, 11
     ecall                   # putchar
+    jal  zero, else_end
     case_c_end:
 
+    addi t3, zero, 120
+    bne  t2, t3, case_x_end # case 'x'
+    addi t2, zero, 0        # capFlag = 0
+    sw   t2, 32(sp)         # store capFlag
+    jal  zero, case_xX_begin
+    case_x_end:
+    addi t3, zero, 88
+    bne  t2, t3, case_xX_end # case 'X'
+    addi t2, zero, 1        # capFlag = 0
+    sw   t2, 32(sp)         # store capFlag
+    case_xX_begin:
+    addi t3, zero, 4        # shiftAmt = 4
+    sw   t3, 36(sp)         # store shiftAmt
+    addi t4, zero, 15       # remain = 15
+    jal  zero, switch_end
+    case_xX_end:
+
+    addi t3, zero, 111
+    bne  t2, t3, case_o_end # case 'o'
+    addi t3, zero, 3        # shfitAmt = 3
+    sw   t3, 36(sp)         # store shiftAmt
+    addi t4, zero, 7        # remain = 7
+    jal  zero, switch_end
+    case_o_end:
+
+    addi t3, zero, 98
+    bne  t2, t3, case_b_end # case 'b'
+    addi t3, zero, 1        # shiftAmt = 1
+    sw   t3, 36(sp)         # store shiftAmt
+    addi t4, zero, 1        # remain = 7
+    jal  zero, switch_end
+    case_b_end:
+    
+    addi t3, zero, 37
+    bne  t2, t3, case_%_end # case '%'
+    addi a0, zero, 37
+    addi a7, zero, 11
+    ecall                   # putchar('%')
     jal  zero, else_end
+    case_%_end:
+
+    addi a0, t2, 0
+    addi a7, zero, 11
+    ecall                   # putchar(chr)
+    jal  zero, else_end
+
+    switch_end:
+    lw   t5, 0(t1)          
+    addi t1, t1, 4          # value = va_arg(ap, unsigned int); 
+    addi t6, zero, 0        # digitIdx = 0
+    parse:
+    and  t2, t5, t4         # digit = (char)(value & remain);
+    srl  t5, t5, t3         # value >> = shiftAmt;
+    addi t2, t2, -10
+    blt  t2, zero, not_Hex  # if(digit > 9)
+    lw   t3, 32(sp)         # load capFlag
+    slli t3, t3, 5          # 0x20 if capFlag == 1
+    xor  t3, t3, zero       # 0x20 if capFlag == 0
+    addi t3, t3, 7          # (capFlag)?0x7:0x27
+    add  t2, t2, t3         # digit += (capFlag)?0x7:0x27;
+    lw   t3, 36(sp)         # load shiftAmt
+    not_Hex:
+    addi t2, t2, 58         # digit + '0' + 10@L:112
+    add  t6, t6, sp         # &digits[digitIdx]
+    sw   t2, 0(t6)          # digits[digitIdx] = digit + '0';
+    sub  t6, t6, sp         # restore digitIdx
+    addi t6, t6, 1          # digitIdx ++
+    ble  t5, zero, dump     
+    addi t2, zero, 32
+    blt  t6, t2, parse      # while(value && digitIdx < sizeof(digits));
+    dump:
+    addi t6, t6, -1         # -- digitIdx
+    add  t6, t6, sp         # &digits[--digitIdx]
+    lb   a0, 0(t6)
+    addi a7, zero, 11
+    ecall                   # putchar(digits[--digitIdx]);
+    sub  t6, t6, sp         # restore digitIdx
+    bgt  t6, zero, dump     # while(digitIdx)
+    jal  zero, else_end
+
     else:                   # else
     addi a0, t2, 0
     addi a7, zero, 11
--- HW1/formatPrinter_test.c
+++ HW1/formatPrinter_test.c
@@ -2,72 +2,84 @@
 #include <stdarg.h>
 
 void self_printf(
-    const char* format,
-    ...
-){
+    const char *format,
+    ...)
+{
     va_list ap;
     va_start(ap, format);
     char chr, *pChr, digit, digits[32];
     int value, shiftAmt = 0, remain, capFlag, digitIdx;
-    while(chr = *format ++) {
-        if(chr == '%'){
-            switch(chr = *format++){
-                case 's':
-                    for(pChr = va_arg(ap, char*);*pChr;pChr++)
-                        putchar(*pChr);
-                    continue;
-                case 'c':
-                    putchar((char)va_arg(ap, int));
-                    continue;
-                case 'x': 
-                    capFlag = 0;
-                case 'X':
-                    capFlag = 1;
-                    shiftAmt = 4;
-                    remain = 15;
-                    break;
-                case 'o':
-                    shiftAmt = 3;
-                    remain = 7;
-                    break;
-                case 'b':
-                    shiftAmt = 1;
-                    remain = 1;
-                    break;
-                case '%':
-                    putchar('%');
-                    continue;
-                default:
-                    putchar(chr);
-                    continue;
+    while (chr = *format++)
+    {
+        if (chr == '%')
+        {
+            switch (chr = *format++)
+            {
+            case 's':
+                for (pChr = va_arg(ap, char *); *pChr; pChr++)
+                    putchar(*pChr);
+                continue;
+            case 'c':
+                putchar((char)va_arg(ap, int));
+                continue;
+            case 'x':
+                capFlag = 0;
+                shiftAmt = 4;
+                remain = 15;
+                break;
+            case 'X':
+                capFlag = 1;
+                shiftAmt = 4;
+                remain = 15;
+                break;
+            case 'o':
+                shiftAmt = 3;
+                remain = 7;
+                break;
+            case 'b':
+                shiftAmt = 1;
+                remain = 1;
+                break;
+            case '%':
+                putchar('%');
+                continue;
+            default:
+                putchar(chr);
+                continue;
             }
             value = va_arg(ap, unsigned int);
             digitIdx = 0;
-            do {
+            do
+            {
                 digit = (char)(value & remain);
-                value >>=shiftAmt;
-                if(digit > 9){
-                    digit += (capFlag)?0x7:0x27;
+                value >>= shiftAmt;
+                if (digit > 9)
+                {
+                    digit += (capFlag) ? 0x7 : 0x27;
                 }
                 digits[digitIdx++] = digit + '0';
-            }while(value && digitIdx < sizeof(digits));
-            do {putchar(digits[--digitIdx]);}while(digitIdx);
-            shiftAmt = 0;
+            } while (value && digitIdx < sizeof(digits));
+            do
+            {
+                putchar(digits[--digitIdx]);
+            } while (digitIdx);
         }
-        else{
+        else
+        {
             putchar(chr);
-        }        
+        }
     }
 }
 
-int main(int argc, char* argv[]){
-    char* pChr = "StringInArg";
+int main(int argc, char *argv[])
+{
+    char *pChr = "StringInArg";
     char chr = 'c';
-    unsigned int    testHex0 = 0x102345,
-                    testHex1 = 0x6789AB,
-                    testHex2 = 0xCDEF,
-                    testOct = 012345670,
-                    testBin = 0b00111011000101001111010100001111;
-    self_printf("%s\nnormalString\nCharInArg%c\nNot supporting: %e%e%r%d\nHex0: %x    %X\nHex1: %x    %X\nHex2: %x    %X\nOct: %o\n", pChr, chr, testHex0, testHex0, testHex1, testHex1, testHex2, testHex2, testOct);
+    unsigned int testHex0 = 0x102345,
+                 testHex1 = 0x6789AB,
+                 testHex2 = 0xCDEF,
+                 testOct = 012345670,
+                 testBin = 0b00111011000101001111010100001111;
+    self_printf("%s\nnormalString\nCharInArg: %c\nPercentSign: %%\nNot supporting: %e%e%r%d\nHex0: %x    %X\nHex1: %x    %X\nHex2: %x    %X\nOct: %o\nBin: %b", pChr, chr, testHex0, testHex0, testHex1, testHex1, testHex2, testHex2, testOct, testBin);
     return 0;
 }
